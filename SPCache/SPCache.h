3eab17d3857cf22d8b39153481f4e4d83186b421
shrinivas.prabhu <shrinivas.prabhu@verse.in>
2018-09-06 11:42:16 +0530

    Initial Commit

---
+++ SPCache/SPCache.h
@@ -0,0 +1,19 @@
+//
+//  SPCache.h
+//  SPCache
+//
+//  Created by Shrinivas Prabhu on 06/09/18.
+//  Copyright © 2018 Shrinivas Prabhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SPCache.
+FOUNDATION_EXPORT double SPCacheVersionNumber;
+
+//! Project version string for SPCache.
+FOUNDATION_EXPORT const unsigned char SPCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPCache/PublicHeader.h>
+
+
---
+++ SPCacheTests/SPCacheTests.swift
@@ -0,0 +1,36 @@
+//
+//  SPCacheTests.swift
+//  SPCacheTests
+//
+//  Created by Shrinivas Prabhu on 06/09/18.
+//  Copyright © 2018 Shrinivas Prabhu. All rights reserved.
+//
+
+import XCTest
+@testable import SPCache
+
+class SPCacheTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
